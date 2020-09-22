705134eabb6c1a22acada93b04e6bf20aad713d0
Gar <gar@lonelydatu.com>
2020-09-22 13:57:47 -0400

    on

--- dev/std_300x250/_bundled/main.js
+++ dev/std_300x250/_bundled/main.js
@@ -13,19 +13,19 @@ function start() {
 	var from = { x: "+=150", rotation: 50, opacity: 0, scale: .3, ease: Back.easeOut };
 	var to = { x: "-=150", y: "-=30", rotation: 50, scale: .7, opacity: .2, ease: Back.easeOut };
 
-	tl.from(".cal_1", .3, _extends({}, from), "+=1");
+	// tl.from(".cal_1", .3, {...from} , "+=1")
 	tl.add("hero2", "+=2");
 
 	tl.to(".cal_1", .6, _extends({}, to), "hero2");
-	tl.from(".cal_2", .6, _extends({}, from), "hero2");
+	// tl.from(".cal_2", .001, {opacity:0}, "hero2")
 	tl.from(".hero2", .01, { opacity: 0 }, "hero2");
 
 	tl.add("hero3", "+=2");
 	tl.to(".cal_2", .6, _extends({}, to), "hero3");
-	tl.from(".cal_3", .6, _extends({}, from), "hero3");
+	// tl.from(".cal_3", .001, {opacity:0}, "hero2-=.1")
 	tl.from(".hero3", .01, { opacity: 0 }, "hero3");
 
-	tl.to(".hero3", .6, { scale: .333333, x: 0, y: 0, ease: Power3.easeOut }, "+=1");
+	tl.to(".hero3", 3, { scale: .333333, x: 0, y: 0, ease: Power4.easeIn }, "hero3");
 
 	tl.set([".hero1", ".hero2"], { opacity: 0 }, "+=2");
 
@@ -38,7 +38,7 @@ function start() {
 	tl.to(".frame2", .3, { opacity: 1 });
 
 	tl.from(".t2", .3, { opacity: 0 });
-	tl.from([".cta", ".logo2"], .3, { opacity: 0 }, "+=.7");
+	tl.from([".cta"], .3, { opacity: 0 }, "+=.7");
 }
 
 start();
--- dev/std_300x250/_js/main.js
+++ dev/std_300x250/_js/main.js
@@ -12,21 +12,21 @@ function start(){
 	const from = {x:"+=150", rotation:50, opacity:0, scale:.3, ease:Back.easeOut}
 	const to = {x:"-=150", y:"-=30", rotation:50, scale:.7, opacity:.2, ease:Back.easeOut}
 
-	tl.from(".cal_1", .3, {...from} , "+=1")
+	// tl.from(".cal_1", .3, {...from} , "+=1")
 	tl.add("hero2", "+=2")
 
 	
 
 	tl.to(".cal_1", .6, {...to}, "hero2")
-	tl.from(".cal_2", .6, {...from}, "hero2")
+	// tl.from(".cal_2", .001, {opacity:0}, "hero2")
 	tl.from(".hero2", .01, {opacity:0}, "hero2")
 
 	tl.add("hero3", "+=2")
 	tl.to(".cal_2", .6, {...to}, "hero3")
-	tl.from(".cal_3", .6, {...from}, "hero3")
+	// tl.from(".cal_3", .001, {opacity:0}, "hero2-=.1")
 	tl.from(".hero3", .01, {opacity:0}, "hero3")
 
-	tl.to(".hero3", .6, {scale:.333333, x:0, y:0, ease:Power3.easeOut}, "+=1")
+	tl.to(".hero3", 3, {scale:.333333, x:0, y:0, ease:Power4.easeIn}, "hero3")
 
 	tl.set([".hero1", ".hero2"], {opacity:0}, "+=2")
 
@@ -40,7 +40,7 @@ function start(){
 	tl.to(".frame2", .3, {opacity:1})
 
 	tl.from(".t2", .3, {opacity:0})
-	tl.from([".cta", ".logo2"], .3, {opacity:0}, "+=.7")
+	tl.from([".cta"], .3, {opacity:0}, "+=.7")
 	
 }
 
