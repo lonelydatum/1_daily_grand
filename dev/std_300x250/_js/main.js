cbde29ac4823c78bcda243de8cb91039a6f74ef2
Gar <gar@lonelydatu.com>
2020-09-23 21:53:26 -0400

    one

--- dev/std_300x250/_js/main.js
+++ dev/std_300x250/_js/main.js
@@ -5,7 +5,7 @@ function start(to_){
 	const tl = new TimelineMax()
 
 	tl.add( first(to_) )
-	tl.to(".hero3", 3, {scale:.333333, x:0, y:0, ease:Power1.easeIn}, "hero3-=1")
+	tl.to(".hero3", 2.5, {scale:.333333, x:0, y:0, ease:Power3.easeIn}, "hero3-=1.5")
 	tl.add( last() )
 	
 	// tl.gotoAndPlay("hero3")	
